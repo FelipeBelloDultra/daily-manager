6a29da17d7db8b49359909ebaca951cb679210c0
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-03-03 10:05:43 -0300

    chore: add alias resolve

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // Components
-import { App } from ".";
+import { App } from "@/.";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
