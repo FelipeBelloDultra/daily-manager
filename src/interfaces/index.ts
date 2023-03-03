d5923d9c221f2905238ed9ed8a0c038dffdc3724
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-03-03 19:59:50 -0300

    feat: create folder for all interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,14 @@
+/**
+ * PARTICIPANTS CONTEXT
+ */
+export interface IParticipants {
+  _id: string;
+  name: string;
+  username?: string;
+  message?: string;
+}
+
+/**
+ * GLOBAL INTERFACES
+ */
+export type { ReactNode as IReactNode } from "react";
