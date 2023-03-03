eb2fbf60d703eb36802ccd3fba2210b0218b4d33
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-03-03 09:59:41 -0300

    setup: create project

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+// Packages
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Components
+import { App } from ".";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />
+);
