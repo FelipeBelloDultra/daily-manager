053dcb85caf800722a79f9e7d4024ea72cc7a1a5
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-03-04 01:07:45 -0300

    feat: implement first style

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 
 // Components
-import { ParticipantItem, Modal } from "~/components";
+import { ParticipantItem, Modal, Button } from "~/components";
 
 // Hooks
 import { useLocalStorage } from "~/hooks/use-local-storage";
@@ -23,14 +23,39 @@ export const App = (): JSX.Element => {
   }
 
   return (
-    <div>
-      <button onClick={() => setShowModal(true)}>Show modal</button>
+    <div className="mx-auto w-full max-w-[900px] pt-12">
+      <header className="flex items-center justify-between px-6 py-7 border-b-2 border-blue-500">
+        <h1 className="text-4xl text-blue-500 font-bold tracking-tighter">
+          Daily <br /> Manager
+        </h1>
 
-      {participants.length
-        ? participants.map((participant) => (
-            <ParticipantItem key={participant._id} participant={participant} />
-          ))
-        : null}
+        <div className="flex flex-col space-y-2">
+          <div className="w-[200px]">
+            <Button onClick={() => setShowModal(true)}>Show modal</Button>
+          </div>
+
+          <div className="w-[120px] self-end">
+            <Button
+              variant="secondary"
+              onClick={() => setParticipants([])}
+              isDisabled={!participants.length}
+            >
+              Reset
+            </Button>
+          </div>
+        </div>
+      </header>
+
+      <main className="flex flex-col gap-2 mt-5 pb-11">
+        {participants.length
+          ? participants.map((participant) => (
+              <ParticipantItem
+                key={participant._id}
+                participant={participant}
+              />
+            ))
+          : null}
+      </main>
 
       <Modal
         isVisible={showModal}
