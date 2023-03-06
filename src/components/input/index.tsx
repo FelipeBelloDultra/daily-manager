5942055cdb910e353fef589af2e0a28d0dc142a9
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-03-06 16:30:31 -0300

    feat(components/input): use forward ref

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,12 +1,9 @@
 // Packages
+import { forwardRef } from "react";
 import classNames from "classnames";
 
 // Interfaces
 import { IInputHTMLAttributes, IReactNode } from "~/interfaces";
-
-// Components
-import InputGroup from "./input-group";
-import InputIcon from "./input-icon";
 
 interface IInputProps extends IInputHTMLAttributes<HTMLInputElement> {
   leftIcon?: IReactNode;
@@ -15,36 +12,37 @@ interface IInputProps extends IInputHTMLAttributes<HTMLInputElement> {
   name: string;
 }
 
-const Input = ({
-  leftIcon,
-  rightIcon,
-  hasError = false,
-  name,
-  type = "text",
-  ...rest
-}: IInputProps): JSX.Element => {
-  const INPUT_CLASSES = classNames(
-    "w-full h-10 rounded-md border-2 shadow-sm px-4 text-black",
-    {
-      "bg-red-50 border-red-700": hasError,
-      "bg-white border-blue-600": !hasError,
-      "pl-[45px]": leftIcon,
-      "pr-[45px]": rightIcon,
-    }
-  );
+const Input = forwardRef<HTMLInputElement, IInputProps>(
+  (
+    { leftIcon, rightIcon, hasError = false, name, type = "text", ...rest },
+    ref
+  ): JSX.Element => {
+    const INPUT_CLASSES = classNames(
+      "w-full h-10 rounded-md border-2 shadow-sm px-4 text-black",
+      {
+        "bg-red-50 border-red-700": hasError,
+        "bg-white border-blue-600": !hasError,
+        "pl-[45px]": leftIcon,
+        "pr-[45px]": rightIcon,
+      }
+    );
 
-  return (
-    <span className="inline-block h-10 w-full relative">
-      {leftIcon ? leftIcon : null}
+    return (
+      <span className="inline-block h-10 w-full relative">
+        {leftIcon ? leftIcon : null}
 
-      <input name={name} type={type} className={INPUT_CLASSES} {...rest} />
+        <input
+          ref={ref}
+          name={name}
+          type={type}
+          className={INPUT_CLASSES}
+          {...rest}
+        />
 
-      {rightIcon ? rightIcon : null}
-    </span>
-  );
-};
-
-Input.InputGroup = InputGroup;
-Input.InputIcon = InputIcon;
+        {rightIcon ? rightIcon : null}
+      </span>
+    );
+  }
+);
 
 export default Input;
