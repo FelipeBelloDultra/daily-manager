40c235155a025252b8a2c12efdc8776ce742210c
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-03-15 11:45:37 -0300

    config: configure some things to run in docker container

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001,
+    host: "0.0.0.0",
+  },
   plugins: [react()],
   resolve: {
     alias: [
